aec5a35a4a705a4c2f93cbc40ddb698d13ebf804
Jamalludeen <jamalghazniwal@gmail.com>
2025-12-24 15:30:19 +0430

    Implement registration view

--- core/serializers.py
+++ core/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password]
     )
-    password2 = serializers.CharField(write_only=True, requried=True)
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
 # Create your views here.
+
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "message": "Account successfully created!",
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response(
+            {"message": "Make sure you provide valid data"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
