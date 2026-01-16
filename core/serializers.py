b74dc948a0678d068ca1de2a3b088d9511d1a080
Jamalludeen <jamalghazniwal@gmail.com>
2026-01-16 17:07:14 +0430

    Override current user serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from djoser.serializers import UserSerializer as BaseUserSerializer
+
 
 User = get_user_model()
+
+
+class DjoserUserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
--- secureAuthenticationSystem/settings.py
+++ secureAuthenticationSystem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,5 +134,13 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('JWT',)
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+}
+
+
+DJOSER = {
+    'SERIALIZERS': {
+        'current_user': 'core.serializers.DjoserUserSerializer'
+    }
 }
